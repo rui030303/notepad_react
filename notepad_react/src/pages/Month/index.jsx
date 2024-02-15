6065cb13b473919aa01b3a59c6aba3fa947aae03
rui030303 <rllu2@student.unimelb.edu.au>
2024-02-15 10:29:55 +0800

    add month result

--- notepad_react/src/pages/Month/index.jsx
+++ notepad_react/src/pages/Month/index.jsx
@@ -1,13 +1,44 @@
 import { NavBar, DatePicker, Toast } from 'antd-mobile'
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './index.scss'
 import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _, { filter,reduce } from 'lodash'
 
 const Month = () => {
+    //get data
+    const billList = useSelector(state=>state.billStore.billList)
+    const monthGroup = useMemo(()=>{
+        return _.groupBy(billList,(item)=>dayjs((item.date)).format('YYYY-MM'))
+    },[billList])
+
+ 
     // time visible
     const now = new Date()
     const [dateVisible, setDateVisible] = useState(false)
     const [currentDate, setCurrentDate] = useState(()=>{return dayjs(now).format('YYYY-MM')})
+    const [currentMonthList,setCurrentMonthList] = useState([])
+    const onConfirm = (val)=>{
+        setCurrentDate(val.toDateString())
+        const currentMonthGroup = monthGroup[dayjs(val).format('YYYY-MM')]
+        setCurrentMonthList(currentMonthGroup)
+    }
+    const monthResult = useMemo(()=>{
+      //calculate
+      if (currentMonthList){
+      const income = currentMonthList.filter(item=>item.type === 'income').reduce((a,c)=>a+c.money, 0)
+      const pay = currentMonthList.filter(item=>item.type === 'pay').reduce((a,c)=>a+c.money, 0)
+      const balance = income + pay
+      return {income, pay, balance}}
+      else{return {income:0, pay:0, balance:0}}
+    },[currentMonthList])
+
+
+    useEffect(()=>{
+      const nowDate = dayjs(now).format('YYYY-MM')
+      monthGroup[nowDate]? setCurrentMonthList(monthGroup[nowDate]) : setCurrentMonthList([])
+    },[monthGroup])
+
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -25,15 +56,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
               <span className="type">expend</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.income.toFixed(2)}</span>
               <span className="type">income</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.balance.toFixed(2)}</span>
               <span className="type">balance</span>
             </div>
           </div>
@@ -48,8 +79,7 @@ const Month = () => {
                 setDateVisible(false)
               }}
             onConfirm={val => {
-                Toast.show(val.toDateString())
-                setCurrentDate(val.toDateString())
+                onConfirm(val)
             }}
           />
         </div>
